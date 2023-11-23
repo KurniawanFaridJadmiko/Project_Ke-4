088d07d1a8bf29e7e7574600a67d334306c94129
KurniawanFaridJadmiko <kurniawan.farid@students.utdi.ac.id>
2023-11-23 11:09:09 +0700

    yah

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://localhost:3000',
+  url: 'http://192.168.100.128:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
